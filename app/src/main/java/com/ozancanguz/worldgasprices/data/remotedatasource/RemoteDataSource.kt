fc9a7501431b2e169a9c11c20d01fd2b4cb9b51f
Ozan <ozancangu24@gmail.com>
2023-01-24 14:00:54 +0300

    -eu request function created in remote data source

--- app/src/main/java/com/ozancanguz/worldgasprices/data/remotedatasource/RemoteDataSource.kt
+++ app/src/main/java/com/ozancanguz/worldgasprices/data/remotedatasource/RemoteDataSource.kt
@@ -1,6 +1,7 @@
 package com.ozancanguz.worldgasprices.data.remotedatasource
 
 import com.ozancanguz.worldgasprices.data.api.GasPriceApi
+import com.ozancanguz.worldgasprices.data.models.eu.EuModel
 import com.ozancanguz.worldgasprices.data.models.usa.Usa
 import retrofit2.Response
 import javax.inject.Inject
@@ -11,6 +12,10 @@ class RemoteDataSource@Inject constructor(private val gasPriceApi: GasPriceApi){
 
     suspend fun getUsaData(): Response<Usa> {
         return gasPriceApi.getUsaPrice()
+    }
+
+    suspend fun getEuData():Response<EuModel>{
+        return gasPriceApi.getEuData()
     }
 
 }
