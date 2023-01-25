882c3f32e4830ba67ee607666b16521a4a9dc4cd
Ozan <ozancangu24@gmail.com>
2023-01-25 13:11:19 +0300

    -tr gasoline screen spinner dropdown menu created

--- app/src/main/java/com/ozancanguz/worldgasprices/ui/tr/gasoline/TrGasolineFragment.kt
+++ app/src/main/java/com/ozancanguz/worldgasprices/ui/tr/gasoline/TrGasolineFragment.kt
@@ -5,9 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import com.ozancanguz.worldgasprices.R
 import com.ozancanguz.worldgasprices.databinding.FragmentTrGasolineBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_tr_gasoline.*
 
 @AndroidEntryPoint
 class TrGasolineFragment : Fragment() {
@@ -25,7 +27,14 @@ class TrGasolineFragment : Fragment() {
          _binding = FragmentTrGasolineBinding.inflate(inflater, container, false)
         val view = binding.root
 
+          var arrayAdapter=ArrayAdapter.createFromResource(
 
+              requireContext(),R.array.brands,
+              android.R.layout.simple_spinner_item
+          )
+
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.spinner3.adapter=arrayAdapter
 
 
         return view
