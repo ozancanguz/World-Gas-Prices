5e8c1f9025a87f19f4ac9c06bbbcf9a225f5217b
Ozan <ozancangu24@gmail.com>
2023-01-25 17:52:19 +0300

    -tr lpg screen request data function created on RemoteDataSource.kt

--- app/src/main/java/com/ozancanguz/worldgasprices/data/remotedatasource/RemoteDataSource.kt
+++ app/src/main/java/com/ozancanguz/worldgasprices/data/remotedatasource/RemoteDataSource.kt
@@ -4,6 +4,7 @@ import com.ozancanguz.worldgasprices.data.api.GasPriceApi
 import com.ozancanguz.worldgasprices.data.models.eu.EuModel
 import com.ozancanguz.worldgasprices.data.models.tr.diesel.TrDieselModel
 import com.ozancanguz.worldgasprices.data.models.tr.gasoline.TrGasolineModel
+import com.ozancanguz.worldgasprices.data.models.tr.lpg.TrLpgModel
 import com.ozancanguz.worldgasprices.data.models.usa.Usa
 import retrofit2.Response
 import javax.inject.Inject
@@ -26,6 +27,10 @@ class RemoteDataSource@Inject constructor(private val gasPriceApi: GasPriceApi){
 
     suspend fun getTrDieselData(city: String):Response<TrDieselModel>{
         return gasPriceApi.getTrDieselData(city)
+    }
+
+    suspend fun getTrLpgPrices(city: String):Response<TrLpgModel>{
+        return gasPriceApi.getTrLpgData(city)
     }
 
 }
