8e043e6e19316fb2dcddbd71ea23ab0eb58d59cb
Ozan <ozancangu24@gmail.com>
2023-01-25 17:41:51 +0300

    -tr lpg screen dropdown menu created

--- app/src/main/java/com/ozancanguz/worldgasprices/ui/tr/lpg/TrLpgFragment.kt
+++ app/src/main/java/com/ozancanguz/worldgasprices/ui/tr/lpg/TrLpgFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import com.ozancanguz.worldgasprices.R
 import com.ozancanguz.worldgasprices.databinding.FragmentTrLpgBinding
 
@@ -26,6 +27,15 @@ class TrLpgFragment : Fragment() {
 
          _binding = FragmentTrLpgBinding.inflate(inflater, container, false)
         val view = binding.root
+
+        var arrayAdapter= ArrayAdapter.createFromResource(
+
+            requireContext(),R.array.brands,
+            android.R.layout.simple_spinner_item
+        )
+
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.trlpgspinner.adapter=arrayAdapter
 
 
 
